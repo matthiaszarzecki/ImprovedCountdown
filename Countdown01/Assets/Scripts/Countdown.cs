5c83aa3fee47e8d813e94846ced8678b88ecd0eb
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2017-03-31 00:16:17 +0200

    initial commit of entire project

---
+++ Countdown01/Assets/Scripts/Countdown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Countdown : MonoBehaviour {
+
+	float timer = 120f;
+
+	void Update (){
+		if (timer > 0f){
+			timer -= Time.deltaTime;
+		} else {
+			timer = 0f;
+		}
+		
+		if (timer < 10f) {
+			GetComponent<TextMesh>().color = Color.red;
+		} else if (timer < 20f) {
+			GetComponent<TextMesh>().color = Color.yellow;
+		}
+		
+		GetComponent<TextMesh>().text = timer.ToString("F2");
+	}
+}
